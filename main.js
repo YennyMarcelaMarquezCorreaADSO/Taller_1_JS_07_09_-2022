ea12df1cf65f8bf05d6785352dfae32971984aeb
MiguelAngelCastroEscamillaINSTRUCTOR <macastro40@misena.edu.co>
2022-09-07 08:47:22 -0500

    Ejercicio 5

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 4. Escribir un programa que calcule el área de un triángulo:
-    // Base = 7 altura = 4 área del triángulo = (base * altura)/2
+    // 5. Escribir un programa que calcule la longitud y el área de una circunferencia: Radio = 4
+    // Longitud de la circunferencia = 2 * PI * radio
+    // Área de la circunferencia = PI * radio˄2
 
 
-    let base = Number(prompt("Ingrese la base", 7));
-    let altura = Number(prompt("Ingrese la altura", 4));
+    let radio = Number(prompt("Ingrese el radio de la circunferencia", 5));
 
-    let triangulo =(base * altura)/2 ;
-    console.log(`%cFormula (${base} * ${altura}) / 2`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-    console.log(`El área del triangulo es : "${triangulo}"`);
 
+    console.group(`%cFormula ${2} * ${Math.PI} * ${radio}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+
+        console.log(`Longitud de la circunferencia : "${2 * Math.PI * radio}"`);
+    console.groupEnd();
+
+    console.group(`%cFormula ${Math.PI} * ${Math.pow(radio, 2)}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+        console.log(`Área de la circunferencia : "${Math.PI * Math.pow(radio, 2)}"`);
+    console.groupEnd();
 })
