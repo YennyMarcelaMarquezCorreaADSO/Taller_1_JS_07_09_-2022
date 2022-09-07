4707965bbe792370cbfeac0daf226d679b50c61a
YennyMarcelaMarquezCorreaADSO <yennycorrea0911@gmail.com>
2022-09-07 11:57:45 -0500

    Ejercicio 11

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Escribir un algoritmo que permita obtener las raíces reales de la ecuación de segundo grado: A * x2 + b * x + c, siendo X un valor constante.
-   
-   
-
-    let A = Number(prompt("Ingrese un valor para A: "));
-    let b = Number(prompt("Ingrese un valor para b: "));
-    let c = Number(prompt("Ingrse un valor para c"))
+    // Escribir un programa que calcule el volumen de un elipsoide
+    // V = (4/3) * PI * a * b *c
     
    
-    console.group(`%cFormula ${-b}-(${Math.sqrt(Math.pow(b, 2))}-${Math.sqrt(4)} * ${Math.sqrt(A)}* ${Math.sqrt(c)}) / ${2} * ${A}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+   
 
-        console.log(`x: "${- b -(Math.sqrt(Math.pow(b, 2)) - Math.sqrt(4) * Math.sqrt(A) * Math.sqrt(c)) / 2 * A }"`);
+    let a = Number(prompt("Ingrese un valor para a: "));
+    let b = Number(prompt("Ingrese un valor para b: "));
+    let c = Number(prompt("Ingrese un valor para c: "))
+    
+   
+    console.group(`%cFormula (${4/3}) * ${Math.PI} * ${a} * ${b}* ${c}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+
+        console.log(`El volumen del elipsoide es de: "${(4/3) * Math.PI * a * b* c}"`);
     console.groupEnd();
 
-    console.group(`%cFormula ${-b} + (${Math.sqrt(Math.pow(b, 2))} - ${Math.sqrt(4)} * ${Math.sqrt(A)} * ${Math.sqrt(c)}) / ${2} * ${A}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-
-        console.log(`X: ${-b + (Math.sqrt(Math.pow(b, 2)) - Math.sqrt(4) * Math.sqrt(A)* Math.sqrt(c)) / 2 * A} ""`);
-    console.groupEnd();
 })
