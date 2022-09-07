cdd3e29fe509054a449299e731a8ef1e408f86d6
YennyMarcelaMarquezCorreaADSO <yennycorrea0911@gmail.com>
2022-09-07 10:33:51 -0500

    Ejercicio 6

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 5. Escribir un programa que calcule la longitud y el área de una circunferencia: Radio = 4
-    // Longitud de la circunferencia = 2 * PI * radio
-    // Área de la circunferencia = PI * radio˄2
+    // Escribir un programa que calcule la velocidad de un proyectil que recorre 2 Km en 5 minutos. 
+    // Expresarel resultado en metros/segundo. 
+    // Velocidad = espacio/tiempo.
+
+    let espacio = 2000;
+    let tiempo = 300;
+    
+    let velocidad = espacio/tiempo
 
 
-    let radio = Number(prompt("Ingrese el radio de la circunferencia", 5));
+    console.group(`%cFormula ${espacio} / ${tiempo}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
 
-
-    console.group(`%cFormula ${2} * ${Math.PI} * ${radio}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-
-        console.log(`Longitud de la circunferencia : "${2 * Math.PI * radio}"`);
+        console.log(`Velocidad del proyectil en m/s: "${velocidad}"`);
     console.groupEnd();
 
-    console.group(`%cFormula ${Math.PI} * ${Math.pow(radio, 2)}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-        console.log(`Área de la circunferencia : "${Math.PI * Math.pow(radio, 2)}"`);
+    console.group(`%cFormula ${espacio} / ${tiempo}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+        console.log(`Velocidad del proyectil en m/s: "${velocidad}"`);
     console.groupEnd();
 })
