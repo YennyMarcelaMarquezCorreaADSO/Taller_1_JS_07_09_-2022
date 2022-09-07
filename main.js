312113328ee2e87692b14533c39fff798c07af41
MiguelAngelCastroEscamillaINSTRUCTOR <macastro40@misena.edu.co>
2022-09-07 08:36:47 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 2. Escribir un programa que calcule el área de un rectángulo:
-    // lado1 = 3 lado2 = 4 área del rectángulo= lado1 * lado2
-    let lado1 = Number(prompt("Ingrese el lado 1", 3));
-    let lado2 = Number(prompt("Ingrese el lado 2", 4));
-    let rectangulo = lado1 * lado2;
-    console.log(`El área del 'rectángulo' es: "${rectangulo}"`);
+    // 3. Un programa que lea 4 números y calcule la media. 
+    // Media= (num1 + num2 + num3 + num4)/4 
+
+    let num1 = Number(prompt("Ingrese el numero 1", 1));
+    let num2 = Number(prompt("Ingrese el numero 2", 2));
+    let num3 = Number(prompt("Ingrese el numero 3", 3));
+    let num4 = Number(prompt("Ingrese el numero 4", 4));
+    let Media = (num1 + num2 + num3 + num4)/4 ;
+    console.log(`%cFormula (${num1} + ${num2} + ${num3} + ${num4}) / 4`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+    console.log(`El promedio es : "${Media}"`);
 
 })
