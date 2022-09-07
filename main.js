d09f36f8a86b07f613fae628f73eafca0da76bbf
YennyMarcelaMarquezCorreaADSO <yennycorrea0911@gmail.com>
2022-09-07 11:49:17 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 9. Escribir un programa que calcule el área y el volumen de un cilindro:
-    // A = (2 * (PI * r˄2)) + ((2 * PI * r) * h)
-    // V = (PI * r2) * h
+    // Escribir un algoritmo que permita obtener las raíces reales de la ecuación de segundo grado: A * x2 + b * x + c, siendo X un valor constante.
    
    
 
-    let r = Number(prompt("Ingrese el valor del radio: "));
-    let h = Number(prompt("Ingrese el valor de la altura: "));
+    let A = Number(prompt("Ingrese un valor para A: "));
+    let b = Number(prompt("Ingrese un valor para b: "));
+    let c = Number(prompt("Ingrse un valor para c"))
     
    
-    console.group(`%cFormula (${2}*(${Math.PI}*${Math.pow(r, 2)}))+((${2}*${Math.PI}*${r})*${h} )`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+    console.group(`%cFormula ${-b}-(${Math.sqrt(Math.pow(b, 2))}-${Math.sqrt(4)} * ${Math.sqrt(A)}* ${Math.sqrt(c)}) / ${2} * ${A}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
 
-        console.log(`El área del cilindro es: "${(2*(Math.PI*Math.pow(r, 2)))+((2*Math.PI*r)*h)}"`);
+        console.log(`x: "${- b -(Math.sqrt(Math.pow(b, 2)) - Math.sqrt(4) * Math.sqrt(A) * Math.sqrt(c)) / 2 * A }"`);
     console.groupEnd();
 
-    console.group(`%cFormula (${Math.PI}*${Math.pow(r, 2)})*${h}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+    console.group(`%cFormula ${-b} + (${Math.sqrt(Math.pow(b, 2))} - ${Math.sqrt(4)} * ${Math.sqrt(A)} * ${Math.sqrt(c)}) / ${2} * ${A}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
 
-        console.log(`El volumen del cilindro es: "${(Math.PI*Math.pow(r, 2))*h}"`);
+        console.log(`X: ${-b + (Math.sqrt(Math.pow(b, 2)) - Math.sqrt(4) * Math.sqrt(A)* Math.sqrt(c)) / 2 * A} ""`);
     console.groupEnd();
 })
