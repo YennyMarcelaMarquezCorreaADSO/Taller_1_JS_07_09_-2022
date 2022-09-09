eece0023ce6e8f9566bc96a6e6fd8d8cdb0a35b0
YennyMarcela MárquezCorreaADSO <yennycorrea0911@gmail.com>
2022-09-08 22:55:02 -0500

    Ejercicio 17

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Ejercicio 16. Elaborar un algoritmo para calcular el promedio final de la materia de algoritmos. Dicha calificación se compone de los siguientes porcentajes:
-    // • 55% del promedio final de sus calificaciones de los tres(3) parciales.
-    // • 30% de la calificación examen final y
-    // • 15% de la calificación trabajo final.
+// Ejercicio 16. Dado el valor que un cliente paga por un producto, calcular qué valor corresponde al costo total del
+// producto y cuánto es el valor del IVA. Considerando que el porcentaje del IVA puede variar en el
+// tiempo y de un producto a otro, este dato se lee por teclado.
+
+
+    let precio =  Number(prompt("Precio total del producto: "))
+    let iva =  Number(prompt("Ingrese el porcentaje de iva: "))
+    let precioiva = precio * (iva/100)
+    let precioTotal = precio + precioiva
+
     
+    console.log(`valor  de iba: "${iva}"`);
+    console.group(`%cFormula (${precio} + ${precioiva})`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
 
-    let nota1 =  Number(prompt("Ingrese la nota1: "))
-    let nota2 =  Number(prompt("Ingrese la nota2: "))
-    let nota3 =  Number(prompt("Ingrese la nota3: "))
-    let examenFinal =  Number(prompt("Ingrese la nota del examen final: "))
-    let trabajoFinal =  Number(prompt("Ingrese la nota el trabajo final: "))
-    let suma = (nota1 + nota2 + nota3)/3
-    let promedio1 = (suma * 0.55)
-    let promedio2 = (examenFinal * 0.3)
-    let promedio3 = (trabajoFinal * 0.15)
-    let promedio = (promedio1 + promedio2 + promedio3)
-
-    console.group(`%cFormula (${promedio1} + ${promedio2} + ${promedio3})`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-
-        console.log(`Promedio de la materia de algoritmo: "${promedio}"`);
+        console.log(`Precio total del producto: "${precioTotal}"`);
     console.groupEnd();
 })
